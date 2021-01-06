e40d4ebbbbe76b8a68d82e4d5d831cc71354bff2
Daniel Soares <daniel.soares@me.com>
2021-01-06 19:37:55 -0300

    feat: add redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/blog',
+        permanent: true
+      }
+    ]
+  },
   images: {
     domains: ['images.unsplash.com']
   }
